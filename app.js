bee7dc2de05a89f2a7fa73f9dc2a7d7ab6d2c42c
LeonardoVileela <leovilela.empresa@gmail.com>
2020-12-21 20:13:32 -0300

    sessão express

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const redis = require('redis')
+const session = require('express-session')
+ 
+let RedisStore = require('connect-redis')(session)
+let redisClient = redis.createClient()
+
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
@@ -12,6 +18,20 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
+app.use(
+  session({
+    store: new RedisStore({ 
+      client: redisClient,
+      host: 'localhost',
+      port: 6379
+    }),
+    secret: 'yuumi',
+    resave: true,
+    saveUninitialized: true
+  })
+)
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/admin.js
+++ routes/admin.js
@@ -28,6 +28,9 @@ router.get('/emails', function (req, res, next) {
 
 router.get('/login', function (req, res, next) {
 
+    if (!req.session.views) { req.session.views = 0 }
+    console.log(req.session.views++)
+
     res.render('admin/login', {
     });
 
